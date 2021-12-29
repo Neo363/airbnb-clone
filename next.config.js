e712cf7b46b131abc20184206f737ba77e5efa55
Niraj Gurung <nirajguroong363@gmail.com>
2021-12-29 14:16:58 +0545

    map integration

--- next.config.js
+++ next.config.js
@@ -5,5 +5,8 @@ module.exports = {
       'links.papercut.com',
       'links.papareact.com'
     ]
+  },
+  env: {
+    mapbox_key: 'pk.eyJ1IjoibmVvZ2ciLCJhIjoiY2t4cjV4cDc5MDFpYzJwcGhoN2gzcXVjdSJ9.aplY7M6Xbah1syUNGPlJLA'
   }
-}
+};
--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ const progress = new ProgressBar({
 
 Router.events.on('routeChangeStart', progress.start);
 Router.events.on("routeChangeComplete", progress.finish);
-Router.events.on("routeChangeError", progress.finish);
+Router.events.on("routeChangeError ", progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
